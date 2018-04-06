046eac0a8c056c352116b52789e853df62b1225c
Nikita Andreiev <nickandreev47@gmail.com>
2018-04-06 18:00:10 +0300

    commit 2

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,20 +1,38 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {NavLink} from 'react-router-dom';
 
-const NavBar = () => (
-    <div className="navbarCl">
-		<div className="navBarImg">
-			<h1 className="navBarTitle">facebook media viewer</h1>
-		</div>
-        <nav>
-			<ul>
-				<li><NavLink exact to='/profile'>Profile</NavLink></li>
-				<li><NavLink exact to='/albums'>Albums</NavLink></li>
-				<li><NavLink exact to='/upload'>Upload</NavLink></li>
-				<li><a className="logoutButton"><span className="glyphicon glyphicon-user"></span>Log Out</a></li>
-			</ul>
-      	</nav>
-    </div>
-);
+class NavBar extends Component{
+	constructor(props) {
+		super(props);
+		this.state = {		
+		};
+
+		this.logoutFunction = this.logoutFunction.bind(this);
+	}
+
+	logoutFunction() {
+		window.FB.logout(function(response) {
+			console.log("function Log Out Runs");
+		});
+	}
+	
+	render(){
+		return (
+			<div className="navbarCl">
+				<div className="navBarImg">
+					<h1 className="navBarTitle">facebook media viewer</h1>
+				</div>
+				<nav>
+					<ul>
+						<li><NavLink exact to='/profile'>Profile</NavLink></li>
+						<li><NavLink exact to='/albums'>Albums</NavLink></li>
+						<li><NavLink exact to='/upload'>Upload</NavLink></li>
+						<li><a className="logoutButton" onClick={ this.logoutFunction }><span className="glyphicon glyphicon-user"></span>Log Out</a></li>
+					</ul>
+				</nav>
+			</div>
+		);
+	}
+};
 
 export default NavBar;  
