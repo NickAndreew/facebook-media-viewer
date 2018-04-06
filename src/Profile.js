dd7f1ab43609665088efc201b8c55df280bea09d
Nikita Andreiev <nickandreev47@gmail.com>
2018-04-06 22:09:56 +0300

    commit 2

--- src/Profile.js
+++ src/Profile.js
@@ -1,22 +1,26 @@
-import React from 'react';
+import React, {Component} from 'react';
 import NavBar from './NavBar';
 
-const Profile = () => (
-    <div>
-        <NavBar />
-        <div>PROFILE</div>
-        <div className="profilePage">
+class Profile extends Component {
+    render(){
+        return(
             <div>
-                <img src="https://i.imgur.com/6oDNbeN.gif" alt="" />
+                <NavBar />
+                <div>PROFILE</div>
+                <div className="profilePage">
+                    <div>
+                        <img src="https://i.imgur.com/6oDNbeN.gif" alt="" />
+                    </div>
+                    <div className="profileInfo">
+                        <h1 className="infoH1">Firstname Lastname</h1>
+                        <h4 className="infoH4">Birthday</h4>
+                        <h4 className="infoH4">Current City</h4>
+                        <h4 className="infoH4">Hometown</h4>
+                    </div>
+                </div>
             </div>
-            <div className="profileInfo">
-                <h1 className="infoH1">Firstname Lastname</h1>
-                <h4 className="infoH4">Birthday</h4>
-                <h4 className="infoH4">Current City</h4>
-                <h4 className="infoH4">Hometown</h4>
-            </div>
-        </div>
-    </div>
-);
+        );
+    }
+};
 
 export default Profile;
