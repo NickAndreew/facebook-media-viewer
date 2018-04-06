dd77682392e0f8ba50ed7bb7ef2cd535e28b9436
Nikita Andreiev <nickandreev47@gmail.com>
2018-04-06 22:30:20 +0300

    commit 2

--- src/Main.js
+++ src/Main.js
@@ -51,6 +51,7 @@ class Main extends Component {
     }
 
     render(){
+        console.log(this.state.status);
         return (
             <Switch>
                 <Route path='/login' component={Login}></Route>
