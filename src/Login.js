0845e53a79b8a022964917922724879dd1493845
Nikita Andreiev <nickandreev47@gmail.com>
2018-04-06 17:33:58 +0300

    commit 2

--- src/Login.js
+++ src/Login.js
@@ -1,18 +1,39 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const Login = () => (
-    <div className="loginBackDiv">
-        <div className="loginFormCl">
-            <div className="logInTexts">
-                <h3 className="logInText">Login</h3>
-                <p className="logInText">This web site is designed for Facebook Users only.</p>
-                <img src="https://i.imgur.com/6oDNbeN.gif" className="logoCl" alt="" />
+class Login extends Component {
+    constructor(props) {
+		super(props);
+		this.state = {
+            status:""		
+		};
+
+		this.loginFunction = this.loginFunction.bind(this);
+    }
+    
+    loginFunction() {
+		window.FB.login(function(response){
+			console.log("function Log In Runs");
+			console.log(response);
+			
+		});
+	}
+
+    render(){
+        return (
+            <div className="loginBackDiv">
+                <div className="loginFormCl">
+                    <div className="logInTexts">
+                        <h3 className="logInText">Login</h3>
+                        <p className="logInText">This web site is designed for Facebook Users only.</p>
+                        <img src="https://i.imgur.com/6oDNbeN.gif" className="logoCl" alt="" />
+                    </div>
+                    <div>
+                        <button className="" onClick={ this.loginFunction } >Login</button>
+                    </div>
+                </div>
             </div>
-            <div>
-                <button className="">Login</button>
-            </div>
-        </div>
-    </div>
-);
+        );
+    }
+};
 
 export default Login;
--- src/Main.js
+++ src/Main.js
@@ -1,17 +1,72 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Profile from './Profile';
 import Albums from './Albums';
 import Upload from './Upload';
 import Login from './Login';
 
-const Main = () => (
-    <Switch>
-        <Route exact path='/login' component={Login}></Route>
-        <Route exact path='/profile' component={Profile}></Route>
-        <Route path='/albums' component={Albums}></Route>
-        <Route path='/upload' component={Upload}></Route>
-    </Switch>
-);
+class Main extends Component {
+    constructor(props) {
+		super(props);
+		this.state = {
+			response: {}
+		};
+    }
+    
+    componentDidMount(){
+        window.fbAsyncInit = function() {
+
+			window.FB.init({
+                appId      : '165205184192174',
+                cookie     : true,
+                xfbml      : true,
+                version    : 'v2.12'
+            });
+			
+            console.log("component did mount func runs");
+            
+        };
+
+        this.getLoginStatusFunction();
+        
+        (function(d, s, id){
+			var js, fjs = d.getElementsByTagName(s)[0];
+			if (d.getElementById(id)) return;
+			js = d.createElement(s);
+			js.id = id;
+			js.src = "https://connect.facebook.net/en_US/sdk.js";
+			fjs.parentNode.insertBefore(js, fjs);
+		}(document, 'script', 'facebook-jssdk'));
+    }
+    
+    getLoginStatusFunction(){
+		console.log("login status func runs");
+
+		window.FB.getLoginStatus(function(response) {
+			if(response.status === "connected"){
+				console.log("user is logged in");
+				// console.log(this);
+				this.loadPageElementsFunction();
+			
+			} else {
+				console.log("user is not logged in");
+				document.getElementById("mainPageDiv").setAttribute("style", "display: none");
+				document.getElementById("albumsDiv").innerHTML = "";
+				document.getElementById("coverPhoto").setAttribute("src", "");
+			}
+		}.bind(this), true);
+	}
+
+    render(){
+        return (
+            <Switch>
+                <Route exact path='/login' component={Login}></Route>
+                <Route exact path='/profile' component={Profile}></Route>
+                <Route path='/albums' component={Albums}></Route>
+                <Route path='/upload' component={Upload}></Route>
+            </Switch>
+        );
+    }
+};
 
 export default Main;
