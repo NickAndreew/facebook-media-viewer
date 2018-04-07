a5e5cec575b807f07b63d7d74f7f63f99b75bfe7
Nikita Andreiev <nickandreev47@gmail.com>
2018-04-07 19:55:28 +0300

    commit

--- src/Album.js
+++ src/Album.js
@@ -28,8 +28,8 @@ class Album extends Component {
             console.log(resp.data);
             for(var i=0 ; i <= resp.data.length-1 ; i++){
                 window.FB.api('/'+resp.data[i].id+'/picture', function(response){
-                    console.log(response.data);
-                    photosList.push(response.data.url);
+                    console.log(response);
+                    photosList.push(response.url);
                 }.bind(this));
             }
 
