b4c18fdc5a0b1e21affa6ba90836942cb4bbd120
Nikita Andreiev <nickandreev47@gmail.com>
2018-04-07 16:07:23 +0300

    commit

--- src/Profile.js
+++ src/Profile.js
@@ -2,6 +2,19 @@ import React, {Component} from 'react';
 import NavBar from './NavBar';
 
 class Profile extends Component {
+
+    componentDidMount(){
+        window.FB.api('/me?fields=id,name,birthday,location,hometown,cover,picture.width(200)',  function(resp) {
+            console.log(resp);
+            document.getElementById("mainPageDiv").setAttribute("style", "display: flex");
+            document.getElementById("profileName").textContent = resp.name;
+            document.getElementById("birthdayDate").textContent = "Birthday date: "+resp.birthday;
+            document.getElementById("currentCity").textContent = "Current city:  "+resp.location.name;
+            document.getElementById("hometown").textContent = "Hometown: "+resp.hometown.name;
+            document.getElementById("profilePic").setAttribute("src", resp.picture.data.url);
+        });
+    }
+
     render(){
         return(
             <div>
@@ -12,10 +25,10 @@ class Profile extends Component {
                         <img src="https://i.imgur.com/6oDNbeN.gif" alt="" />
                     </div>
                     <div className="profileInfo">
-                        <h1 className="infoH1">Firstname Lastname</h1>
-                        <h4 className="infoH4">Birthday</h4>
-                        <h4 className="infoH4">Current City</h4>
-                        <h4 className="infoH4">Hometown</h4>
+                        <h1 className="infoH1" id="profileName">Firstname Lastname</h1>
+                        <h4 className="infoH4" id="birthdayDate">Birthday</h4>
+                        <h4 className="infoH4" id="currentCity">Current City</h4>
+                        <h4 className="infoH4" id="hometown">Hometown</h4>
                     </div>
                 </div>
             </div>
