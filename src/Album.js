0ea5dab5fe8b68d54d80093d039d31a7a437cc16
Nikita Andreiev <nickandreev47@gmail.com>
2018-04-07 18:44:47 +0300

    commit

--- src/Album.js
+++ src/Album.js
@@ -1,22 +1,41 @@
-import React, {Component} from 'react';
+import React from 'react';
 import AlbumsAPI from './AlbumsAPI';
 import {Link} from 'react-router-dom';
 
-class Album extends Component {
-    constructor(props) {
-		super(props);
-		this.state = {
-		}
-    }
-    
-    render (){
-        return (
-            <div>
-                <h1>{this.props.name} (#{this.props.id})</h1>
-                <Link to='/albums'><h4 className="h4Cl">Back</h4></Link>
-            </div>
-        )
-    }
+const Album = (props) => {
+    const albumId = props.match.params.id;
+    const albumName = props.match.params.name;
+      
+    return (
+        <div>
+            <h1>{albumName} (#{albumId})</h1>
+            <Link to='/albums'><h4 className="h4Cl">Back</h4></Link>
+        </div>
+    )
 }   
 
 export default Album;
+
+
+// import React, {Component} from 'react';
+// import AlbumsAPI from './AlbumsAPI';
+// import {Link} from 'react-router-dom';
+
+// class Album extends Component {
+//     constructor(props) {
+// 		super(props);
+// 		this.state = {
+// 		}
+//     }
+    
+//     render (){
+//         return (
+//             <div>
+//                 <h1>{this.props.name} (#{this.props.id})</h1>
+//                 <Link to='/albums'><h4 className="h4Cl">Back</h4></Link>
+//             </div>
+//         )
+//     }
+// }   
+
+// export default Album;
--- src/AllAlbums.js
+++ src/AllAlbums.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import AlbumsAPI from './AlbumsAPI';
 import {Link} from 'react-router-dom';
 
 class AllAlbums extends Component {
