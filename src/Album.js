093f7621021df8d665312c5dc6ddc1a2944baedd
Nikita Andreiev <nickandreev47@gmail.com>
2018-04-08 15:42:24 +0300

    commit

--- src/Album.js
+++ src/Album.js
@@ -40,7 +40,7 @@ class Album extends Component {
     }
     
     render (){
-        // console.log(this.state.photos);
+        console.log(this.state.photos);
         
         if(this.state.photos.length>0){
             console.log(this.state.photos);
