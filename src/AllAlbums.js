78432099e5596246fd14c50d0fbd40bb7d4535f5
Nikita Andreiev <nickandreev47@gmail.com>
2018-04-10 01:02:09 +0300

    commit

--- src/AllAlbums.js
+++ src/AllAlbums.js
@@ -25,8 +25,8 @@ class AllAlbums extends Component {
                 
                 // var cover = '';
 
-                const resp = window.FB.api('/'+id+'/picture?redirect=false');
-                const cover = resp.data.url;
+                const responce = window.FB.api('/'+id+'/picture?redirect=false');
+                const cover = responce.data.url;
                 // , function(response){
                 console.log(cover);                   
                     // cover = response.data.url;
