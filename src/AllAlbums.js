b8ef17d4465a51457fa81a144d9ede094c985c8b
Nikita Andreiev <nickandreev47@gmail.com>
2018-04-09 22:31:24 +0300

    commit

--- src/AllAlbums.js
+++ src/AllAlbums.js
@@ -26,6 +26,7 @@ class AllAlbums extends Component {
 
                 window.FB.api('/'+responce.albums.data[i].id+'/picture?redirect=false', async function(response){
                     obj.cover = await response.data.url;
+                    console.log("ID : "+obj.data.id+", Name : "+obj.data.name+", Cover : "+obj.cover);
                     albumsList.push(obj);
                 }.bind(this));
                 // obj.cover = await url;
