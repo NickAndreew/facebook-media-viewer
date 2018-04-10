ff51c65ce89ee79ea41e6db300fd3d066df4f5d1
Nikita Andreiev <nickandreev47@gmail.com>
2018-04-10 20:52:13 +0300

    commit

--- src/UploadTo.js
+++ src/UploadTo.js
@@ -3,13 +3,48 @@ import AlbumsAPI from './AlbumsAPI';
 import {Link} from 'react-router-dom';
 
 class UploadTo extends Component {
+    constructor(props) {
+		super(props);
+		this.state = {
+            albums: []
+        }
+    }
+
+    componentDidMount(){
+        window.FB.api('/me?fields=albums', function(resp) {
+            
+            var albumsList = [];
+
+            for (var i=0 ; i <= resp.albums.data.length-1 ; i++) {
+                var data = resp.albums.data[i];
+                var id = data.id;
+                console.log(id);
+                
+                var cover = '';
+
+                // const responce = window.FB.api('/'+id+'/picture?redirect=false', async function(response){                   
+                //     return response.data.url;
+                // });
+                cover = this.getAlbumCover(id);
+                console.log(cover);
+
+                albumsList.push(data);
+
+            }
+
+            console.log(albumsList);
+            this.setState({ albums : albumsList });
+        
+        }.bind(this));
+    }
+    
     render() {
         return (
             <div>
                 {
                     AlbumsAPI.all().map(a => (
-                    <div key={a.number}>
-                        <Link to={`/upload/${a.number}`}>
+                    <div key={a.id}>
+                        <Link to={`/upload/${a.id}`}>
                             <h3 className="h4Cl">{a.name}</h3>    
                         </Link>
                     </div>
