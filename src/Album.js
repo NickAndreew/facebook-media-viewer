f956b97525e1e58669ce2ab6303d3cfb23c315eb
Nikita Andreiev <nickandreev47@gmail.com>
2018-04-11 15:44:56 +0300

    commit

--- src/Album.js
+++ src/Album.js
@@ -41,7 +41,7 @@ class Album extends Component {
     
     render (){
         // console.log(this.state.photos);
-        
+        console.log(this.props.match.params.name);
         if(this.state.photos.length>0){
             console.log(this.props.match.params.name);
             return (
