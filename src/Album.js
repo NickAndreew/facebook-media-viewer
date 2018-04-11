6a37b145df9db377897197b8061ad7659744a809
Nikita Andreiev <nickandreev47@gmail.com>
2018-04-11 16:47:25 +0300

    commit

--- src/Album.js
+++ src/Album.js
@@ -16,6 +16,8 @@ class Album extends Component {
         window.FB.api('/'+this.props.match.params.id, function(resp){
             if(resp.name!=''){
                 var name = resp.name;
+                console.log(name);
+                console.log(resp);
                 this.setState({albumName: name});
             } else {
                 this.setState({albumName: 'Undefined'});
